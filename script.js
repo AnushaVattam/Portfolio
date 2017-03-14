8264a5d05795da50c3a772daf2b151a1e015f4c2
AnushaVattam <anusha.v403@gmail.com>
2017-03-14 14:28:53 -0500

    Online portfolio version 1

---
+++ script.js
@@ -0,0 +1,14 @@
+<input type="button" value="button name" onclick="myFunction()"/>
+<script>
+function myFunction() {
+   <object src="Resume2017.pdf" type="application/x-pdf" title="ResumePdf" width="500" height="720">
+    <a href="Resume2017.pdf">Resume</a> 
+</object>
+}
+</script>
+
+</body>
+</html>
+
+
+
